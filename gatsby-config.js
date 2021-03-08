c53cb2f71307199a072017e0785bb9c49f2f6ebe
Anna Roos <anna.flytstrom@gmail.com>
2021-03-08 11:54:23 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "ci-lecture",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
